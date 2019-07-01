6dcad85622e4f864a8fb220d49ce7f4921424944
Aaron Peikert <aaron.peikert@posteo.de>
2019-07-01 18:28:18 +0200

    update site

---
+++ docs/R/build_site.R
@@ -0,0 +1,11 @@
+#!/usr/bin/Rscript --vanilla
+if(!require("pacman"))install.packages("pacman")
+pacman::p_load("here", "rmarkdown", "git2r")
+
+#----build---
+rmarkdown::render_site(here())
+
+#----commit---
+add(here(), here("docs"))
+commit(here(), "update site")
+push(here())
